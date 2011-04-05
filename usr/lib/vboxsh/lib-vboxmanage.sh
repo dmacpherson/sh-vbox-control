53e6f262a2f91570d53b13d89920139c39093741
OrionFyre <orionfyre@hotmail.com>
2011-04-05 09:43:24 -0500

    vmlist

--- usr/lib/vboxsh/lib-vboxmanage.sh
+++ usr/lib/vboxsh/lib-vboxmanage.sh
@@ -21,7 +21,7 @@ gen_vm_list ()
       state=`echo "$state" | sed 's/^ *//;s/ *$//'`
       echo "\"$vmname\" \"$state\" \\" >> $TMPDIR/vmlist
       echo "my name is: ${vmname} and my state is: ${state}"
-      vm_list=$vm_list" \""$vmname"\" \""$state
+      vm_list=$vm_list" "$vmname" "$state
       
    done
    echo "-----------------"
