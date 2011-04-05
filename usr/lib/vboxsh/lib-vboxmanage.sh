d6fb486d95e52e6496cca0f78032577d63962426
OrionFyre <orionfyre@hotmail.com>
2011-04-05 04:14:10 -0500

    vm-list

--- usr/lib/vboxsh/lib-vboxmanage.sh
+++ usr/lib/vboxsh/lib-vboxmanage.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 ###### vboxsh VirtualBox Management Library ######
-touch $
+
 vbox_list_vms ()
 {
-vm_list=$(VBoxManage list vms | sort | sed 's/[\{\}]/\"/g')
+vm_longlist=$(VBoxManage list vms --long)
+vm_list=""
+for name in $(grep "Name: " | sed s/^Name:[\s+]//g <<$vm_list) 
+do
+    for state in $(grep "State: " | sed s/^State:[\s+]//g <<$vm_list) 
+    do
+        vm_list="$vmlist\"$name\" \"$state\"" 
+    done
+done
+
+
+
 echo $vm_list >> /tmp/vm-manage.log
 exit
 }
