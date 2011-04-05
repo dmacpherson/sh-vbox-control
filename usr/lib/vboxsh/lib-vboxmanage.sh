522ed9e2e480e49e9383e5ec5ca3649d64bf1150
OrionFyre <orionfyre@hotmail.com>
2011-04-05 04:16:36 -0500

    vm-list

--- usr/lib/vboxsh/lib-vboxmanage.sh
+++ usr/lib/vboxsh/lib-vboxmanage.sh
@@ -5,9 +5,9 @@ vbox_list_vms ()
 {
 vm_longlist=$(VBoxManage list vms --long)
 vm_list=""
-for name in $(grep "Name: " | sed s/^Name:[\s+]//g <<$vm_list) 
+for name in $(grep "Name: " <<$vm_list | sed s/^Name:[\s+]//g) 
 do
-    for state in $(grep "State: " | sed s/^State:[\s+]//g <<$vm_list) 
+    for state in $(grep "State: " <<$vm_list | sed s/^State:[\s+]//g) 
     do
         vm_list="$vmlist\"$name\" \"$state\"" 
     done
