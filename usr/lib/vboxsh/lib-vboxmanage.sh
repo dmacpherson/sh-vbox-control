665c6036f835d739b9a2cc525b2971a1d8c342eb
OrionFyre <orionfyre@hotmail.com>
2011-04-05 18:41:48 -0500

    create vm interaction

--- usr/lib/vboxsh/lib-vboxmanage.sh
+++ usr/lib/vboxsh/lib-vboxmanage.sh
@@ -28,80 +28,9 @@ gen_vm_list ()
 
 }
 
-# Generate list of OS types for VM Creation
-# List up to date as of VirtualBox 4
-# Marking 'gen_os_type' Deprecated --OF
-gen_os_type ()
-{
-ask_option 0 
-		"Windows31" "Windows 3.1" \
-                "Windows95" "Windows 95" \
-                "Windows98" "Windows 98" \
-                "WindowsMe" "Windows Me" \
-                "WindowsNT4" "Windows NT4" \
-                "Windows2000" "Windows 2000" \
-                "WindowsXP" "Windows XP" \
-                "WindowsXP_64" "Windows XP (64 bit)" \
-                "Windows2003" "Windows 2003" \
-                "Windows2003_64" "Windows 2003 (64 bit)" \
-                "WindowsVista" "Windows Vista" \
-                "WindowsVista_64" "Windows Vista (64 bit)" \
-                "Windows2008" "Windows 2008" \
-                "Windows2008_64" "Windows 2008 (64 bit)" \
-                "Windows7" "Windows 7" \
-                "Windows7_64" "Windows 7 (64 bit)" \
-                "WindowsNT" "Other Windows" \
-                "Linux24" "Linux Kernel 2.4" \
-                "Linux24_64" "Linux Kernel 2.4 (64 bit)" \
-                "Linux26" "Linux Kernel 2.6" \
-                "Linux26_64" "Linux Kernel 2.6 (64 bit)" \
-                "ArchLinux" "ArchLinux" \
-                "ArchLinux_64" "ArchLinux (64 bit)" \
-                "Debian" "Debian" \
-                "Debian_64" "Debian (64 bit)" \
-                "OpenSUSE" "OpenSUSE" \
-		"Fedora" "Fedora" \
-                "Fedora_64" "Fedora (64 bit)" \
-                "Gentoo" "Gentoo" \
-                "Gentoo_64" "Gentoo (64 bit)" \
-                "Mandriva" "Mandriva" \
-                "Mandriva_64" "Mandriva (64 bit)" \
-                "RedHat" "RedHat" \
-                "RedHat_64" "RedHat (64 bit)" \
-                "Turbolinux" "Turbolinux" \
-                "Ubuntu" "Ubuntu" \
-                "Ubuntu_64" "Ubuntu (64 bit)" \
-                "Xandros" "Xandros" \
-                "Xandora_64" "Xandros (64 bit)" \
-                "Oracle" "Oracle" \
-                "Oracle_64" "Oracle (64 bit)" \
-                "Linux" "Other Linux" \
-                "Solaris" "Solaris" \
-                "Solaris_64" "Solaris (64 bit)" \
-                "OpenSolaris" "OpenSolaris" \
-                "OpenSolaris_64" "OpenSolaris (64 bit)" \
-                "FreeBSD" "FreeBSD" \
-                "FreeBSD_64" "FreeBSD (64 bit)" \
-                "OpenBSD" "OpenBSD" \
-                "OpenBSD_64" "OpenBSD (64 bit)" \
-                "NetBSD" "NetBSD" \
-                "NetBSD_64" "NetBSD (64 bit)" \
-                "OS2Warp3" "OS2Warp3" \
-                "OS2Warp4" "OS2Warp4" \
-                "OS2Warp45" "OS2Warp45" \
-                "OS2eCS" "OS2eCS" \
-                "OS2" "OS2" \
-                "MacOS" "Mac OS" \
-                "MacOS_64" "Mac OS (64 bit)" \
-                "DOS" "DOS" \
-                "Netware" "Netware" \
-                "L4" "L4" \
-                "QNX" "QNX"
-}
-
 
 ### Queuery ostypes known to the version of virtualbox installed
-queuery_vbox_ostypes()
+queuery_vbox_ostypes ()
 {
    VBoxManage list ostypes | grep ':' >$TMPDIR/ostypes
    ### output of the call piped to stdin is in format:
@@ -119,3 +48,12 @@ queuery_vbox_ostypes()
    ask_option 0 "Select an OS Type" '' required "0" "Other" "${OSTYPES[@]}"
 }
 
+worker_create_vm ()
+{
+   VBoxManage -q createvm --name "$cvm_name" --ostype "$cvm_ostype" --register
+   VBoxManage -q modifyvm "$cvm_name" --memory $cvm_mem --acpi on --boot1 dvd --nic1 nat --hwvirtex on --pae on
+   VBoxManage -q storagectl "$cvm_name" --name "IDE Controller" --add ide
+   VBoxManage -q createvdi -filename "/datapool/virtualbox/harddrives/$cvm_name.vdi" -size $cvm_hdd --register
+   VBoxManage -q storageattach "$cvm_name" --storagectl "IDE Controller" --port 0 --device 0 --type hdd --medium "/datapool/virtualbox/harddrives/$cvm_name.vdi"
+   VBoxManage -q storageattach "$cvm_name" --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium "$cvm_iso"
+}   
