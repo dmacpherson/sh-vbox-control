2e4ff5a1328199287b271714d1ca11659e72147e
OrionFyre <orionfyre@hotmail.com>
2011-04-05 05:56:40 -0500

    vmlist

--- usr/lib/vboxsh/lib-vboxmanage.sh
+++ usr/lib/vboxsh/lib-vboxmanage.sh
@@ -3,20 +3,15 @@
 
 vbox_list_vms ()
 {
-vm_list=""
-VBoxManage list vms --long > $TMPDIR/vmlist.tmp
-cat $TMPDIR/vmlist.tmp | grep "Name: " | sed s/^Name:[\s+]//g >$TMPDIR/names.log
-#for name in $(cat $TMPDIR/vmlist.tmp | grep "Name: " | sed s/^Name:[\s+]//g) 
-#do
-    #for state in $(cat $TMPDIR/vmlist.tmp | grep "State: " | sed s/^State:[\s+]//g) 
-    #do
-    #    vm_list="$vmlist\"$name\" \"$state\"" 
-    #done
-#    echo $name >> $TMPDIR/name.log
-#done
-
-
-
-echo $vm_list >> /tmp/vm-manage.log
+   VBoxManage -q list vms | while read line
+   do
+      tmp=${line#*\"}
+      vmname=${tmp%\"*}
+      state=`VBoxManage showvminfo $vmname | grep State`
+      tmp=${state#*\:}
+      state=${tmp%\(*}
+      state=`echo "$state" | tr -s " "`
+      echo "\"$vmname\" \"$state\" \\" >> $TMPDIR/vboxlist.tmp
+   done
 exit
 }
