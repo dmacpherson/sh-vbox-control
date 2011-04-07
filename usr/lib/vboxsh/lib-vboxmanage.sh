b6f086fe73eb31ed6315dae45bc66088016729d9
Denis MacPherson <denismacpherson@gmail.com>
2011-04-07 10:21:36 -0700

    added vboxheadless command

--- usr/lib/vboxsh/lib-vboxmanage.sh
+++ usr/lib/vboxsh/lib-vboxmanage.sh
@@ -132,7 +132,7 @@ worker_snapshot_restore ()
 # Worker to issue start/stop commands to VM
 # $1 (req) signal to send to vm
 # $2 (req) VM to send signal to
-# $3 (opt) headless configuration string
+# $3 (opt) headless configuration string -n -m yourport -o yourpassword
 # TODO: Error handling
 worker_startstop_vm ()
 {
@@ -145,6 +145,7 @@ worker_startstop_vm ()
    then
       sleep 1
       #VBoxHeadless
+      nohup VBoxHeadless -s $2 $3 > /dev/null 2>&1 &
    fi
 }
 
